62678af9f66e89d8f54351cf07f1f57d43abe024
choiys99 <cys1849908@gmail.com>
2024-09-11 20:54:46 +0900

    0911

--- ultimate-react-course-main/02-JS-review/starter/script.js
+++ ultimate-react-course-main/02-JS-review/starter/script.js
@@ -142,3 +142,138 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+//
+// const book = getBook(2);
+
+// // const title = book.title;
+// // const author = book.author;
+
+// const { title, author, pages, publicationDate, hasMovieAdaptation, genres } =
+//   book;
+
+// console.log(title, author, genres);
+
+// // const primaryGenre = genres[0];
+// // const secondaryGenre = genres[1];
+
+// const [primaryGenre, secondaryGenre, ...ohterGenres] = genres;
+
+// console.log(primaryGenre, secondaryGenre, ohterGenres);
+
+// const newGenres = [...genres, "epic fantasy"];
+// newGenres;
+
+// const updatedBook = { ...book, moviePublicationDate: "2001-12-19", pages: 12 };
+// updatedBook;
+
+// const getYear = (str) => str.split("-")[0];
+// console.log(getYear(publicationDate));
+// const summary = `${title}, a {pages}-page long book, was written by ${author} and published in ${getYear}.The book has ${
+//   hasMovieAdaptation ? "" : "not"
+// } been apapted as a movie`;
+// summary;
+
+// const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+// console.log(`The book has ${pagesRange} pages`);
+
+// // function getYear(str) {
+// //   return str.split("-")[0];
+// // }
+
+// console.log(true && "Some string");
+// console.log(false && "Some string");
+// console.log(hasMovieAdaptation && "This book has a movie");
+
+// console.log("jonas" && "Some string");
+// console.log(0 && "some string");
+
+// console.log(true || "some string");
+// console.log(false || "some string");
+
+// console.log(book.translations.spanish);
+
+// const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+// spanishTranslation;
+
+// console.log(book.reviews.librarything.reviewsCount);
+// const countWrong = book.reviews.librarything.reviewsCount || "no data";
+// countWrong;
+
+// const count = book.reviews.librarything.reviewsCount ?? "no data";
+// count;
+
+// function getTotalReviewCount(book) {
+//   const goodreads = book.reviews.goodreads.reviewsCount;
+//   const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+//   return goodreads + librarything;
+// }
+// console.log(getTotalReviewCount(book));
+
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews.goodreads.reviewsCount;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+const books = getBooks();
+books;
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount,
+}));
+essentialData;
+
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((books) => books.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
+
+const pagesAllBooks = books.reduce((acc, book) => acc + book.pages, 0);
+//                                 acc = 누산기 book =현재처리요소  , 0 = 초기값
+pagesAllBooks;
+
+const arr = [3, 7, 1, 9, 6];
+const sorted = arr.slice().sort((a, b) => a - b);
+arr;
+sorted;
+
+const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
+sortedByPages;
+
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chanber of Secrets",
+  author: "J . K Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// console.log(fetch("https://jsonplaceholder.typicode.com/todos"));
+// console.log("Cys");
+
+async function getTodos(params) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+const todos = getTodos();
+console.log(todos);
+
+console.log("Cys");
